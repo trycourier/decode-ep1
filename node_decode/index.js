54fd151d719720019b6db64931825b30e15df63d
mikemilla <mike@mikemiller.design>
2023-02-09 09:18:56 -0800

    More send examples

--- node_decode/index.js
+++ node_decode/index.js
@@ -1,17 +1,21 @@
 const Courier = require('@trycourier/courier')
 
-const courier = Courier.CourierClient({ authorizationToken: "pk_prod_4XPS5PVFEBM48RQCXTK4P0BH1CXJ" });
+const courier = Courier.CourierClient({ authorizationToken: "your_key" });
 
 async function test() {
+
+  const userId = "decode_user"
+  const title = "Hello!"
+  const body = "This message was sent from NodeJS"
 
   const { requestId } = await courier.send({
     message: {
       to: {
-        user_id: "decode_user",
+        user_id: userId,
       },
       content: {
-        title: "Hey!",
-        body: "How are you?",
+        title: title,
+        body: body,
       },
       routing: {
         method: "single",
@@ -19,6 +23,44 @@ async function test() {
       },
     },
   });
+
+  // Example with custom data
+  // const { requestId } = await courier.send({
+  //   message: {
+  //     to: {
+  //       user_id: userId,
+  //     },
+  //     content: {
+  //       title: title,
+  //       body: body,
+  //     },
+  //     routing: {
+  //       method: "single",
+  //       channels: ["firebase-fcm"],
+  //     },
+  //     providers: {
+  //       "firebase-fcm": {
+  //         override: {
+  //           body: {
+  //             data: {
+  //               example_url: "https://docs.courier.com"
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   },
+  // });
+
+  // https://app.courier.com/designer/notifications/S57FS04HNS4Z4GHJ61RHBS1QT69G/design
+  // const { requestId } = await courier.send({
+  //   message: {
+  //     to: {
+  //       user_id: userId,
+  //     },
+  //     template: "S57FS04HNS4Z4GHJ61RHBS1QT69G",
+  //   },
+  // });
 
   console.log(requestId)
 
